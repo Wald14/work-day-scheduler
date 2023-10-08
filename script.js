8e47ba72fecdd7d6d0b3f76b1856369c41458967
Wald14 <lukewald@hotmail.com>
2023-10-08 14:03:38 -0500

    Practice code for making sure the button is clicked and then traversing the DOM to get to the class of the parent element

--- script.js
+++ script.js
@@ -9,13 +9,32 @@ $(function main () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
+  var saveButtonEl = $(".saveBtn");
+  saveButtonEl.on("click", function () {
+    console.log("Save button has been clicked");
+    console.log(this);
+
+    var parentEl = $(this).parent("div");
+    console.log (parentEl);
+
+    // var parentClass= parentEl.getAttribute("class");
+    // console.log(parentClass);
+  });
+
+  // var calendarObj = {
+  //   hour: "9:00",
+  //   task: function() {
+  //     console.log (this.hour);
+  //   }
+  // }
+
+  
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  //
+  
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
