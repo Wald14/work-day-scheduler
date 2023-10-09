6ba5f9b09a5c351b7ccd41ad34bad27da34e427d
Wald14 <lukewald@hotmail.com>
2023-10-09 17:52:59 -0500

    advanced the function to now determine the sibling textareas value (val)

---
+++ README.md
@@ -0,0 +1,7 @@
+
+##Acknowledgements
+
+https://www.w3schools.com/
+https://stackoverflow.com/questions/17109593/how-can-i-get-the-class-of-a-parent-element
+https://stackoverflow.com/questions/3239598/how-can-i-get-the-id-of-an-element-using-jquery
+https://gomakethings.com/how-to-get-the-index-of-an-object-in-an-array-with-vanilla-js/#:~:text=The%20Array.,indexOf('Radagast')%3B
--- script.js
+++ script.js
@@ -2,45 +2,69 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 
-$(function main () {
+$(function main() {
+  var saveButtonEl = $(".saveBtn");
+  var now = dayjs();
+
+  var calendar = [{
+    hour: "hour-9",
+    task: "task for 9am"
+  },
+  {
+    hour: "hour-10",
+    task: "task for 10am"
+  },
+  {
+    hour: "hour-11",
+    task: "task for 11am"
+  }]
+
+  // function searchCalendar(hour, index) {
+  //   if(hour.hour === "hour-10")
+  //     return true;
+  // }
+
+  // var hourSpecificTask = calendar.findIndex(searchCalendar("hour-10", 0))
+  // console.log(hourSpecificTask)
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  var saveButtonEl = $(".saveBtn");
   saveButtonEl.on("click", function () {
-    console.log("Save button has been clicked");
-    console.log(this);
+    // Determines the parent Div's Id (aka hour button clicked)
+    var hourBtnClicked = $(this).parent().attr("id");
+    console.log(`Hour button clicked: ${hourBtnClicked}`);
 
-    var parentEl = $(this).parent("div");
-    console.log (parentEl);
+    // Determines where the matching object is in the calendar 
+    var indexOfHourClicked = calendar.findIndex(function (calendar) {
+      return calendar.hour === hourBtnClicked
+    })
+    console.log (`Index of the hour selected: ${indexOfHourClicked}`)
 
-    // var parentClass= parentEl.getAttribute("class");
-    // console.log(parentClass);
+    // Return the "task" in the hour clicked
+    console.log (calendar[indexOfHourClicked].task)
+
+    // Determine the text in the sibling textarea to the button that was clicked
+    var taskTextArea = $.trim($(this).siblings("textarea").val());
+    console.log(taskTextArea)
+
   });
 
-  // var calendarObj = {
-  //   hour: "9:00",
-  //   task: function() {
-  //     console.log (this.hour);
-  //   }
-  // }
 
-  
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  
+
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  
+
 
   // COMPLETED: Add code to display the current date in the header of the page.
-  var now = dayjs();
   $("#currentDay").text(now.format("dddd, MMM Do"));
 });
