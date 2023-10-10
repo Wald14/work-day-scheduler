4eab9395ca2431bb29e73d2c5b07c30a51f19630
Wald14 <lukewald@hotmail.com>
2023-10-09 20:27:20 -0500

    Added function to load saved calendar OR if no calendar is saved to local storage, generate a new one.

--- script.js
+++ script.js
@@ -6,18 +6,7 @@ $(function main() {
   var saveButtonEl = $(".saveBtn");
   var now = dayjs();
 
-  var calendar = [{
-    hour: "hour-9",
-    task: ""
-  },
-  {
-    hour: "hour-10",
-    task: ""
-  },
-  {
-    hour: "hour-11",
-    task: ""
-  }]
+  var calendar = loadCalendar();
 
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -28,25 +17,14 @@ $(function main() {
   saveButtonEl.on("click", function () {
     // Determines the parent Div's Id (aka hour button clicked)
     var hourBtnClicked = $(this).parent().attr("id");
-    console.log(`Hour button clicked: ${hourBtnClicked}`);
-
     // Determines where the matching object is in the calendar 
     var indexOfHourClicked = calendar.findIndex(function (calendar) {
       return calendar.hour === hourBtnClicked
     })
-    console.log (`Index of the hour selected: ${indexOfHourClicked}`)
-
-    // Return the "task" in the hour clicked
-    console.log (`old task for ${hourBtnClicked} was: ${calendar[indexOfHourClicked].task}`)
-
     // Determine the text in the sibling textarea to the button that was clicked
     var taskTextArea = $.trim($(this).siblings("textarea").val());
-    console.log(`User entered task: ${taskTextArea}`)
-
     // Set or replace task for that hour in the calendar array
     calendar[indexOfHourClicked].task = taskTextArea;
-    console.log(`New task for ${hourBtnClicked} set to: ${calendar[indexOfHourClicked].task}`)
-
     // Call save function to save changes to local storage
     saveCalendar()
   });
@@ -65,6 +43,30 @@ $(function main() {
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
 
+  function loadCalendar() {
+    calendar = JSON.parse(localStorage.getItem("Task-Calendar"));
+
+    if (calendar === null) {
+      return [{
+        hour: "hour-9",
+        task: ""
+    },
+    {
+      hour: "hour-10",
+        task: ""
+    },
+    {
+      hour: "hour-11",
+        task: ""
+
+    }];
+    } else {
+      for (var i = 9; i < 9 + calendar.length; i++) {
+        $("#hour-" + i).children("textarea").text(calendar[i - 9].task);
+      }
+      return calendar;
+    }
+  }
 
   // COMPLETED: Add code to display the current date in the header of the page.
   $("#currentDay").text(now.format("dddd, MMM Do"));
