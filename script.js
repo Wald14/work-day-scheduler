6c75cba5534bbc7870a80e7b66858a16888c00b3
Wald14 <lukewald@hotmail.com>
2023-10-09 23:30:34 -0500

    Added code to apply/remove the past, present, or future class to each time block

--- script.js
+++ script.js
@@ -5,16 +5,24 @@
 $(function main() {
   var saveButtonEl = $(".saveBtn");
   var now = dayjs();
+  var hour = now.hour()
 
   var calendar = loadCalendar();
 
 
-  // TODO: Add a listener for click events on the save button. This code should
+  // function updateNow(){
+  //   now = dayjs();
+  // }
+  // setInterval(updateNow, 1000)
+
+  
+  // COMPLETED: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
+
   saveButtonEl.on("click", function () {
     // Determines the parent Div's Id (aka hour button clicked)
     var hourBtnClicked = $(this).parent().attr("id");
@@ -34,13 +42,41 @@ $(function main() {
     localStorage.setItem("Task-Calendar", JSON.stringify(calendar));
   }
 
-  // TODO: Add code to apply the past, present, or future class to each time
+
+
+
+
+  // Completed: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
 
-  // TODO: Add code to get any user input that was saved in localStorage and set
+  var timeBlocks = $(".time-block")
+
+  for (i = 0; i < timeBlocks.length; i++) {
+    var singleBlock = timeBlocks[i];
+    var blockHour = $(singleBlock).attr('id').replace("hour-", "");
+    if (blockHour < hour) {
+      singleBlock.classList.add("past");
+      singleBlock.classList.remove("present");
+      singleBlock.classList.remove("future");
+    } else if (blockHour > hour) {
+      singleBlock.classList.remove("past");
+      singleBlock.classList.remove("present");
+      singleBlock.classList.add("future");
+    } else {
+      singleBlock.classList.remove("past");
+      singleBlock.classList.add("present");
+      singleBlock.classList.remove("future");
+    }
+  }
+
+
+
+
+
+  // COMPLETED: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
 
@@ -59,37 +95,30 @@ $(function main() {
       {
         hour: "hour-11",
         task: ""
-
       },
       {
         hour: "hour-12",
         task: ""
-
       },
       {
         hour: "hour-13",
         task: ""
-
       },
       {
         hour: "hour-14",
         task: ""
-
       },
       {
         hour: "hour-15",
         task: ""
-
       },
       {
         hour: "hour-16",
         task: ""
-
       },
       {
         hour: "hour-17",
         task: ""
-
       }];
     } else {
       for (var i = 9; i < 9 + calendar.length; i++) {
